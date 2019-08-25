1b5423621219af0015d5c9cbaff1f2782280fff2
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2019-08-25 18:52:06 +0300

    Commit 4 - a random string consisting of 8 numbers is generated

--- ProgrammingDemo/Program.cs
+++ ProgrammingDemo/Program.cs
@@ -10,8 +10,14 @@ namespace ProgrammingDemo
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            Console.WriteLine("Generated random number is " + rand.Next(0, 10));
+            Random generator = new Random();
+            string result = "";
+            for (int i = 1; i <= 8; i++)
+            {
+                var digit = generator.Next(0, 10);
+                result += digit;
+            }
+            Console.WriteLine("Your 8-digit number is " + result + "!");
             Console.ReadKey();
         }
     }
