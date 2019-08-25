1f0a06dec6c126d0eaca5e53effb10cdbbebf8bd
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2019-08-25 19:05:00 +0300

    Commit 5 - The program asks for the number of digits you want to generate.

--- ProgrammingDemo/Program.cs
+++ ProgrammingDemo/Program.cs
@@ -12,12 +12,33 @@ namespace ProgrammingDemo
         {
             Random generator = new Random();
             string result = "";
-            for (int i = 1; i <= 8; i++)
+
+            int numberOfDigits = 0;
+
+            ConsoleKeyInfo UserInput;
+
+            // We check input for a Digit
+            do
+            {
+                Console.Write("How many digits do you want in your number? ==> ");
+                UserInput = Console.ReadKey(); // Get user input
+
+                if (char.IsDigit(UserInput.KeyChar))
+                {
+                    numberOfDigits = int.Parse(UserInput.KeyChar.ToString()); // use Parse if it's a Digit
+                }
+                else
+                {
+                    Console.WriteLine("\nDear user, you are a bit dumb, aren't you? Try again!");
+                }
+            } while (!char.IsDigit(UserInput.KeyChar));
+
+            for (int i = 1; i <= numberOfDigits; i++)
             {
                 var digit = generator.Next(0, 10);
                 result += digit;
             }
-            Console.WriteLine("Your 8-digit number is " + result + "!");
+            Console.WriteLine("\nYour " + numberOfDigits + "-digit number is " + result + "!");
             Console.ReadKey();
         }
     }
