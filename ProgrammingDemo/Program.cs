6bd996fac30cb491eab210403dc12fbb57f49525
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2019-08-25 18:35:21 +0300

    Commit 2 - SImple Hello World Demo in C#

--- ProgrammingDemo/Program.cs
+++ ProgrammingDemo/Program.cs
@@ -10,6 +10,8 @@ namespace ProgrammingDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
