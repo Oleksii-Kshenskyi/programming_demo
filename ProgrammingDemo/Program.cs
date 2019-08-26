730c89bd131d9c1573eb266b0462cf3c80bed958
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2019-08-26 13:13:19 +0300

    Commit 9: - Main menu implemented.

--- ProgrammingDemo/Program.cs
+++ ProgrammingDemo/Program.cs
@@ -10,8 +10,60 @@ namespace ProgrammingDemo
     {
         static void Main(string[] args)
         {
-            PerformCalculations();
+            StartMainMenu();
         }
+
+        static void StartMainMenu()
+        {
+            string choice = "";
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Vadym number generation application!");
+                Console.WriteLine("Please choose what to do:");
+                Console.WriteLine("1. Generate random numbers following direct rules.");
+                Console.WriteLine("2. Generate random numbers following reversed rules.");
+                Console.WriteLine("3. Input a number yourself and let the application verify it.");
+                Console.WriteLine("4. Exit the application.");
+                Console.Write("==> ");
+
+                ConsoleKeyInfo UserInput;
+                UserInput = Console.ReadKey(); // Get user input
+                int temp = 0;
+
+                if (char.IsDigit(UserInput.KeyChar) && int.TryParse(UserInput.KeyChar.ToString(), out temp))
+                {
+                    choice = temp.ToString();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.WriteLine();
+                            PerformCalculations();
+                            break;
+                        case "2":
+                            Console.WriteLine("\nNot Implemented Yet!");
+                            break;
+                        case "3":
+                            Console.WriteLine("\nNot Implemented Yet!");
+                            break;
+                        case "4":
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("\nDear user, you are a bit dumb, aren't you? Try again!");
+                            break;
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("\nDear user, you are a bit dumb, aren't you? Try again!");
+                    Console.ReadKey();
+                }
+            } while (choice != "4");
+        }
+    
 
         static void PerformCalculations()
         {
